d723b392b469787d06f190962c95ecf730dbe0b2
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-21 21:03:21 -0400

    added the validation logic for the response form

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -23,10 +24,23 @@ namespace PartyInvites.Controllers
             return View();
         }
 
-        public ViewResult RsvpForm()
+        [HttpGet] public ViewResult RsvpForm()
         {
-
             return View();
+        }
+
+        [HttpPost] public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
         }
     }
 }
